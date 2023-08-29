0c4f5ce0abad26e2f0309cdb360e54558f1bc987
Sydney Bär <baer.sydney@outlook.com>
2023-08-29 20:50:59 +0200

    Images fit now on whole page with wrap attribute

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // arrays
 let images = ['img/1.webp.jpg', 'img/2.webp.jpg', 'img/3.webp.jpg', 'img/4.webp.jpg', 'img/5.webp.jpg', 'img/6.webp.jpg', 'img/7.webp.jpg', 'img/8.webp.jpg', 'img/9.webp.jpg',
-    'img/10.webp.jpg', 'img/11.webp.jpg'];
+    'img/10.webp.jpg', 'img/11.webp.jpg', 'img/12.webp.jpg', 'img/13.webp.jpg', 'img/14.webp.jpg', 'img/15.webp.jpg'];
 // variables
 
 
@@ -8,12 +8,11 @@ function load() {
     let setImages = document.getElementById('mainImgContainer');
 
     for (i = 0; i < images.length; i++) {
-        // const img = images[i];
-
+        const img = images[i];
 
         setImages.innerHTML += /*html*/`
             <div class="imgBox">
-                <img src="${images[i]}">
+                <img src="${img}">
             </div>
         `;
     }
