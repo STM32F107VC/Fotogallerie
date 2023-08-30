6628d386ca4c371d6e9d290eaa7e50d4e4cfe07c
Sydney Bär <baer.sydney@outlook.com>
2023-08-30 18:26:34 +0200

    hide direction arrow previous or next picture if last one is reached

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// arrays
+// array
 let images = ['img/1.webp.jpg', 'img/2.webp.jpg', 'img/3.webp.jpg', 'img/4.webp.jpg', 'img/5.webp.jpg', 'img/6.webp.jpg', 'img/7.webp.jpg', 'img/8.webp.jpg', 'img/9.webp.jpg',
     'img/10.webp.jpg', 'img/11.webp.jpg', 'img/12.webp.jpg', 'img/13.webp.jpg', 'img/14.webp.jpg', 'img/15.webp.jpg', 'img/16.webp.jpg', 'img/17.webp.jpg', 'img/18.webp.jpg', 'img/19.webp.jpg'];
 
 // variables
-
+let i;
 
 // Load all pictures out of array
 function load() {
@@ -12,7 +12,7 @@ function load() {
     setImages.classList.add('mainImgContainer');
     setImages.classList.remove('increaseImg');
 
-    for (let i = 0; i < images.length; i++) {
+    for (i = 0; i < images.length; i++) {
         const img = images[i];
         setImages.innerHTML += /*html*/`
                 <div onclick="openImg(${i})" class="imgBox">
@@ -37,9 +37,9 @@ function templateRender(i) {
     return /*html*/`
     <div class="popUpImg">
         <img class="img back" onclick="load()" src="img/icons8-back-50.png" alt="back">
-        <img  onclick="previousImg(${i})" class="img" src="img/icons8-back-26.png" alt="previous back">
+        <img id="previousPicture" onclick="previousImg(${i})" class="img" src="img/icons8-back-26.png" alt="previous back">
         <img src="${images[i]}">
-        <img onclick="nextImg(${i})" class="img"src="img/icons8-forward-26.png" alt="next picture">
+        <img id="nextPicture" onclick="nextImg(${i})" class="img"src="img/icons8-forward-26.png" alt="next picture">
     </div>
 `;
 }
@@ -50,8 +50,11 @@ function previousImg(i) {
     if (!(i < 1)) { // Check that i isn't smaller than array length
         i -= 1;
         openImg(i);
-    } else {
-        templateRender();
+
+        if (i == 0) {
+            removeArrowLeft('leftArrow');
+            // templateRender();
+        }
     }
 }
 
@@ -61,8 +64,26 @@ function nextImg(i) {
     if (!(i > 17)) { // Check that i isn't bigger than array length 
         i += 1;
         openImg(i);
-    } else {
-        console.log('value range isnt correct!');
-        templateRender();
+
+        if (i == 18) {
+            removeArrowRight('rightArrow');
+            // templateRender();
+        }
+    }
+}
+
+// Hide left arrow if first picture is reached
+function removeArrowLeft(leftArrow) {
+    let arrowBehindPrevious = document.getElementById('previousPicture');
+    if (leftArrow) {
+        arrowBehindPrevious.classList.add('hideArrow');
+    }
+}
+
+// Hide right arrow if last picture is reached
+function removeArrowRight(rightArrow) {
+    let arrowBehindNext = document.getElementById('nextPicture');
+    if (rightArrow) {
+        arrowBehindNext.classList.add('hideArrow');
     }
 }
